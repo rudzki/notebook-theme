2fe209a269c41efd452c9717bb700c856be2cc83
Chris Rudzki <chris@rudzki.org>
2017-12-10 00:03:01 -0500

    Remove extra widgets from archive page

--- 404.php
+++ 404.php
@@ -18,41 +18,14 @@ get_header(); ?>
 			<?php } ?>
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'notebook' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Oy vey! That page can&rsquo;t be found.', 'notebook' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'notebook' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'notebook' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'notebook' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					<p>
+						<?php esc_html_e( 'It looks like nothing was found at this location. Head back to the ', 'notebook' ); ?>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">front page</a>.
+					</p>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
