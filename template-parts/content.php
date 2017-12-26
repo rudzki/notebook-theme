058e6745b2f2463a098bada8a0071fbc9117a18a
Chris Rudzki <chris@rudzki.org>
2017-12-26 09:44:01 -0500

    Entry content margin

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,10 +17,7 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-		<?php
-		endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
 		<div class="entry-content">
